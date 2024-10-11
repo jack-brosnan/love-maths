b5cd6c07e8407e96a07161d79161274e7233beb7
Jack Brosnan <ik7pdsoq@students.codeinstitute.net>
2024-10-11 19:29:44 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
             else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
@@ -20,11 +22,19 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 
-let runGame = () => {
+let runGame = (gameType) => {
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }
+    else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 
 }
 
@@ -44,8 +54,10 @@ let incrementWrongAnswer = () => {
     
 }
 
-let displayAdditionQuestion = () => {
-    
+let displayAdditionQuestion = (operand1, operand2) => {
+    document.getElementById("operand1").innerText = operand1;
+    document.getElementById("operand2").innerText = operand2;
+    document.getElementById("operator").innerText = "+";
 }
 
 let displaySubtractionQuestion = () => {
